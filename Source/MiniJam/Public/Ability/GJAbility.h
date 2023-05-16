a578e86f119d901b40e06a7dfdb3d0628389d4e6
Maikel van Munsteren <mogitu@msn.com>
2023-05-16 22:18:26 +0200

    Skeleton classes added for abilty and weapon framework.

---
+++ Source/MiniJam/Public/Ability/GJAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "GJAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINIJAM_API UGJAbility : public UObject
+{
+	GENERATED_BODY()
+	
+};
